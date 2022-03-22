78ed686f7b2bb51512cd66c5574d8c7122d0240c
Jonathan <jonathan@bangert.dk>
2022-03-22 15:27:39 +0100

    Kasjd

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     private Rigidbody playerRigidbody;
+    public Vector2 direction;
     private bool touching;
-    public float jumpSpeed = 10f;
+    public float speed = 20;
+    public float jumpSpeed = 20;
     private void Awake() {
         playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate() {
+        playerRigidbody.AddForce(transform.forward * speed * Time.deltaTime * direction.y * 100);
+        playerRigidbody.AddForce(transform.right * speed * Time.deltaTime * direction.x * 100);
+    }
+    public void Move(InputAction.CallbackContext context) {
+        direction = context.ReadValue<Vector2>();
     }
     public void Jump() {
         if(touching){
