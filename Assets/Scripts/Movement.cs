c06e24671ca264315aef6573c3257e2c9fc6ec96
Jonathan <jonathan@bangert.dk>
2022-03-26 11:06:19 +0100

    Comments in code

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,27 +5,34 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    // Variables
     private Rigidbody playerRigidbody;
-    public Vector2 direction;
+    private Vector2 direction;
     private bool touching;
     public float speed = 20;
     public float jumpSpeed = 20;
+
+    // Awake (called before Start)
     private void Awake() {
         playerRigidbody = GetComponent<Rigidbody>();
     }
 
+    // Fixed Update (Physics)
     public void FixedUpdate() {
         playerRigidbody.AddForce(transform.forward * speed * Time.deltaTime * direction.y * 100);
         playerRigidbody.AddForce(transform.right * speed * Time.deltaTime * direction.x * 100);
     }
-    public void Move(InputAction.CallbackContext context) {
+    // UpdateDirection (called by Input)
+    public void UpdateDirection(InputAction.CallbackContext context) {
         direction = context.ReadValue<Vector2>();
     }
+    // Jump (called by Input)
     public void Jump() {
         if(touching){
             playerRigidbody.AddForce(transform.up * jumpSpeed, ForceMode.Impulse);
         }
     }
+    // Checks if the player is touching the ground
     private void OnCollisionEnter(Collision collision){touching = true;}
     private void OnCollisionExit(Collision collision){touching = false;}
 }
